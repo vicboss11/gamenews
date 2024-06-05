1b01722fd6937d951188bacf28ae33ffad15dffb
Víctor <victor.af1104@gmail.com>
2024-06-06 00:35:53 +0200

    Add dotenv dependency and update .gitignore, astro.config.mjs, package.json and pnpm-lock.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from "astro/config";
+import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
 
-import cloudflare from "@astrojs/cloudflare";
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: cloudflare(),
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly WP_SITE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+    otherLocals: {
+      test: string;
+    };
+  }
+}
