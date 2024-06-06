5bf941f0a0fb7539fd93ec1405fe50516c16238e
Víctor <victor.af1104@gmail.com>
2024-06-07 01:35:24 +0200

    Add replace-shortcodes.ts file with functions for handling shortcodes

---
+++ src/scripts/replace-shortcodes.ts
@@ -0,0 +1,78 @@
+import type { Heading } from '../models/interfaces/html';
+import type {
+  Shortcode,
+  ShortcodeAttributes,
+} from '../models/interfaces/shortcodes';
+
+export function getShortcodesFromHtml(html: string): Shortcode[] {
+  const shortcodes: Shortcode[] = [];
+  const shortcodeRegex = /\[(\w+)(.*?)\]/g;
+
+  let match;
+
+  while ((match = shortcodeRegex.exec(html)) !== null) {
+    const name = match[1];
+    const attributesString = match[2].trim();
+
+    const shortcodeAttributes: ShortcodeAttributes = {};
+    const attrRegex = /(\w+)=['"](.*?)['"]/g;
+
+    let attrMatch;
+
+    while ((attrMatch = attrRegex.exec(attributesString)) !== null) {
+      shortcodeAttributes[attrMatch[1]] = attrMatch[2];
+    }
+
+    shortcodes.push({
+      name,
+      attributes: shortcodeAttributes,
+      original: match[0],
+      index: match.index,
+    });
+  }
+
+  return shortcodes;
+}
+
+export function updateTocShortcodeWithHeadings(
+  shortcodes: Shortcode[],
+  headings: Heading[],
+) {
+  return shortcodes.map((shortcode) =>
+    shortcode.name === 'ToC'
+      ? {
+          ...shortcode,
+          attributes: {
+            headings: JSON.stringify(headings),
+          },
+        }
+      : shortcode,
+  );
+}
+
+export function getComponentFromShortcode(shortcode: Shortcode): {
+  component: string | null;
+  props: {
+    [key: string]: string | null;
+  };
+} {
+  switch (shortcode.name) {
+    case 'GameNews':
+      return {
+        component: 'GameNews',
+        props: {
+          id: shortcode.attributes.id, // TODO: Add attributes
+        },
+      };
+    case 'ToC':
+      return {
+        component: 'TableOfContents',
+        props: {
+          title: shortcode.attributes?.title || null,
+          headings: shortcode.attributes?.headings || null,
+        },
+      };
+    default:
+      return { component: null, props: {} };
+  }
+}
