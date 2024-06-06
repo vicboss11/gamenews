51dca8d5626d0f2c375ebddc1354dbf358c7f7f7
Víctor <victor.af1104@gmail.com>
2024-06-07 01:29:14 +0200

    Add WordPress interfaces for pages, posts, authors, and categories

---
+++ src/models/interfaces/wordpress.ts
@@ -0,0 +1,38 @@
+export interface WpPage {
+  date: string;
+  modified: string;
+  slug: string;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+  };
+}
+
+export interface WpPost extends WpPage {
+  author: number;
+  categories: number[];
+  tags: string[];
+  _embedded: {
+    author: WpAuthor;
+  };
+  'wp:term': WpCategory[];
+}
+
+export interface WpAuthor {
+  id: number;
+  name: string;
+  description: string;
+  avatar_urls: {
+    24: string;
+    48: string;
+    96: string;
+  };
+}
+
+export interface WpCategory {
+  name: string;
+  slug: string;
+  taxonomy: 'category';
+}
