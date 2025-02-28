a2209e2bfc16e58c5a1203c430848101c8ad0620
Víctor <victor.af1104@gmail.com>
2025-02-28 18:41:27 +0100

    Rename env variable

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,6 @@ export const {
   NODE_ENV = 'local',
   APP_NAME = 'GameNews',
   GAMENEWS_API,
-  GAMENEWS_TOKEN,
+  GAMENEWS_API_TOKEN,
   WP_SITE,
 } = import.meta.env || process.env;
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,24 +1,13 @@
-/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
   readonly NODE_ENV: string;
   readonly APP_NAME: string;
   readonly GAMENEWS_API: string;
-  readonly GAMENEWS_TOKEN: string;
+  readonly GAMENEWS_API_TOKEN: string;
   readonly WP_SITE: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
-}
-
-type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
-
-declare namespace App {
-  interface Locals extends Runtime {
-    otherLocals: {
-      test: string;
-    };
-  }
 }
